74a9cbf8b2ef61408d5a8080beffa9f817487b44
Carlin Kartchner <carlin.kartchner@gmail.com>
2023-03-29 08:43:15 -0400

    load csv into displayed dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
+import pandas
 import streamlit
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('Breakfast Favorites')
 streamlit.header('🥣 Breakfast Menu')
@@ -7,3 +10,4 @@ streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
