764859e35a86fd627fef440be2731cf3a88b704e
Carlin Kartchner <carlin.kartchner@gmail.com>
2023-03-29 08:46:01 -0400

    define fruit picker index

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import pandas
 import streamlit
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title('Breakfast Favorites')
 streamlit.header('🥣 Breakfast Menu')
@@ -10,4 +11,6 @@ streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+# Not sure I love the "vendor lock-in" of this type of app
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
